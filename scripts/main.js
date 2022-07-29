8b01d1a403888961603d2561a6d6596d0f063a3b
unknown <nguyenduyhoang_t64@hus.edu.vn>
2022-07-29 11:25:04 +0700

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,165 @@
+const express = require('express');
+const axios = require('axios');
+const { Builder, By, Key, until, JavascriptExecutor, Capabilities } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const { fbFriendsCrawler, } = require('./facebook_friends.js');
+const { getAllZaloGroups } = require('./zalo_groups.js');
+const { getSpecificGroupMembers } = require('./getGroupMembers.js');
+const csvReader = require('csv-parser');
+const fs = require('fs');
+const cors = require('cors');
+const path = require('path');
+const { json } = require('express');
+const app = express();
+const port = 8888;
+
+//middleware
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('E:/Projects/Selenium/src'))
+
+
+//Route 
+app.get('/', (req, res) => {
+	res.sendFile('E:/Projects/Selenium/src/index.html')
+});
+
+
+app.post('/fb-friends', (req, res) => {
+	let data = new Array();
+	fbFriendsCrawler(req.body.email, req.body.password, "friend", data).then(friends => {
+		let htmls = friends.map(friend => {
+			return `
+			<div class="friend-list">
+				
+				<div class="row border-bottom mt-2">
+					<div class="col-sm-2">
+						${friend.index}
+					</div>
+					<div class="col-sm-4 font-weight-bold">
+						${friend.name}
+					</div>
+					<div class="col-sm-6">
+						<a href="${friend.link}">${friend.link}</a>
+					</div>
+				</div>
+				
+			</div>`
+		});
+		let html = htmls.join('');
+		let header = `	<div class="row border-bottom mt-2">
+							<div class="col-sm-2 font-weight-bold">
+								No.
+							</div>
+							<div class="col-sm-4 font-weight-bold">
+								Name
+							</div>
+							<div class="col-sm-6 font-weight-bold">
+								FB Link
+							</div>
+						</div>`
+		res.send(`<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+					<h3 class= "mt-5 mb-5 text-center text-uppercase font-weight-bold">Result</h3>
+					<div class="container mb-5">`+ header + html + `</div>`);
+	}).catch((err) => {
+		console.error(err);
+		res.sendFile('E:/Projects/Selenium/src/404.html');
+	}
+	);
+	// res.send(req.body);
+});
+
+app.get('/fb-friends', (req, res) => {
+	res.sendFile('E:/Projects/Selenium/src/crawl-friends-fb.html');
+})
+
+
+app.get('/zalo-specific-group', (req, res) => {
+	res.sendFile('E:/Projects/Selenium/src/crawl-gr-members-zalo.html');
+	// res.send('Specific Group members');
+});
+
+app.post('/zalo-specific-group', (req, res) => {
+	let data;
+	console.log(req.body);
+	getSpecificGroupMembers(req.body.group, data)
+		.then((data) => {
+			let htmls = data.members.map(member => {
+				return `
+				<div class="row border-bottom mt-2">
+					<div class="col-sm-2">
+						${data.members.indexOf(member)}
+					</div>
+					<div class="col-sm-10 text-primary">
+						${member}
+					</div>
+				
+				</div>`
+			});
+			let html = htmls.join('');
+			let header = `<h3 class="mt-5 mb-5">Group: ${data.gr_name} </h3>`
+			res.send(`<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+		integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+						<h3 class= "mt-5 mb-5 text-center text-uppercase font-weight-bold">Result</h3>
+						<div class="container mb-5">`+ header + `<ul class="p-4">${html}</ul>` + `</div>`);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.sendFile('E:/Projects/Selenium/src/404.html');
+		});
+});
+
+
+
+app.get('/zalo-groups', (req, res) => {
+	res.sendFile('E:/Projects/Selenium/src/crawl-groups-zalo.html');
+
+});
+
+app.post('/zalo-groups', (req, res) => {
+	let data;
+	getAllZaloGroups(data).then(groups => {
+		console.log(groups);
+		// res.json(groups);
+		let htmls = groups.map(group => {
+			return `
+			<div class="friend-list">
+				
+				<div class="row border-bottom mt-2">
+					<div class="col-sm-2">
+						${group.index}
+					</div>
+					<div class="col-sm-4 font-weight-bold">
+						${group.gr_name}
+					</div>
+					<div class="col-sm-6">
+						${group.participants}
+					</div>
+				</div>
+				
+			</div>`
+		});
+		let html = htmls.join('');
+		let header = `	<div class="row border-bottom mt-2">
+							<div class="col-sm-2 font-weight-bold">
+								No.
+							</div>
+							<div class="col-sm-4 font-weight-bold">
+								Group Name
+							</div>
+							<div class="col-sm-6 font-weight-bold">
+								Number of participants
+							</div>
+						</div>`
+		res.send(`<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+		integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+						<h3 class= "mt-5 mb-5 text-center text-uppercase font-weight-bold">Result</h3>
+						<div class="container mb-5">`+ header + html + `</div>`)
+	});
+})
+
+app.listen(port, () => {
+	console.log(`Crawler app listening on port ${port}`)
+});
---
+++ scripts/zaloNameCheck.js
@@ -0,0 +1,115 @@
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const fs = require('fs');
+const csvParser = require('csv-parser');
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+const { elementLocated } = require('selenium-webdriver/lib/until');
+
+
+// read data from csv file
+
+function readData(filename, data) {
+    let i = 1;
+    try {
+        fs.createReadStream(filename)
+            .pipe(csvParser())
+            .on('data', function (row) {
+                // console.log(row.phone + '\t' + row.name);
+                data.push({
+                    index: i++,
+                    phone: row.phone,
+                    name: row.name,
+                    zaloName: row.name,
+                });
+                // console.log(data);
+            })
+            .on('end', function () {
+                console.log('File read successfully ');
+            });
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function checkName(contacts, driver) {
+
+}
+
+function writeCsv(data) {
+    try {
+
+        if (!fs.existsSync('E:/Projects/Selenium/data/contacts')) {
+            console.log("Folder does not exist. Creating... ")
+            fs.mkdir('E:/Projects/Selenium/data/contacts', { recursive: true }, err => { });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+    const csvWriter = createCsvWriter({
+        path: `data/contacts/contactsE.csv`,
+        header: [
+            { id: "index", title: "index" },
+            { id: "phone", title: "phone" },
+            { id: "name", title: "name" },
+            { id: "zaloName", title: "zaloName" },
+        ],
+    });
+
+    csvWriter
+        .writeRecords(data)
+        .then(() => console.log("The CSV file was written successfully"));
+}
+
+async function clear(driver, elm) {
+    // await driver.executeScript(e => e.select(), elm);
+    // await elm.sendKeys(Key.BACK_SPACE);
+    await driver.executeScript("arguments[0].value = '';", elm)
+}
+// main process
+async function zaloNameCheck() {
+    let driver = await new Builder().forBrowser('chrome').build();
+    await driver.get('https://chat.zalo.me/');
+    await driver.manage().window().maximize();
+    let data = [];
+    data = readData('E:/Projects/Selenium/data/contacts/contacts.csv', data);
+
+    try {
+
+        if (data.length === 0) {
+            console.log('No phones found');
+        }
+        let findXpath = '//*[@id="contact-search-input"]';
+
+        let search = await driver.wait(until.elementLocated(By.xpath(findXpath)));
+        for (let elm of data) {
+            await clear(driver, search);
+            await driver.sleep(1000);
+
+            await search.sendKeys(elm.phone);
+            await driver.sleep(10000);
+            let conv = await driver.findElements(By.className('conv-item-title__name'));
+
+            if (conv.length > 0) {
+                console.log('check check');
+                elm.zaloName = await driver.wait(until.elementLocated(By.className('conv-item-title__name'))).getText();
+                // await clear(driver, search);
+                console.log('clear');
+            } else {
+                continue;
+            }
+
+
+        }
+        writeCsv(data);
+
+        console.log(data);
+    } catch (errors) {
+        console.log(errors);
+
+    } finally {
+        await driver.close(until);
+    }
+}
+
+
+zaloNameCheck().then().catch(function (err) { console.error(err); });
